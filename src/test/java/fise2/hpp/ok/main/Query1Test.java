b74f34dbc45fa25978a5e623639434e8621bc0b4
PierreTSE <pierre--giraud@orange.fr>
2019-05-16 18:30:31 +0200

    tests unitaires améliorés

--- src/test/java/fise2/hpp/ok/main/Query1Test.java
+++ src/test/java/fise2/hpp/ok/main/Query1Test.java
@@ -17,12 +17,8 @@ public class Query1Test {
         BufferedReader expectedReader = Files.newBufferedReader(Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource(expected)).toURI()));
         BufferedReader actualReader = Files.newBufferedReader(Paths.get(Utils.getJARPath().toString(), actual));
 
-        int i = 0;
         while (expectedReader.ready()) {
-            ++i;
-            if (!expectedReader.readLine().equals(actualReader.readLine())) {
-                return false;
-            }
+            Assert.assertEquals(expectedReader.readLine(), actualReader.readLine());
         }
         return true;
     }
@@ -31,76 +27,76 @@ public class Query1Test {
     public void Q1Basic() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Basic", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Basic/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1Basic2() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Basic2", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Basic2/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1BigTest() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1BigTest", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1BigTest/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1Case1() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Case1", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Case1/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1Case2() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Case2", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Case2/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1Case3() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Case3", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Case3/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1Case4() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Case4", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Case4/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1Case5() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1Case5", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1Case5/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1CommentCount() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1CommentCount", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1CommentCount/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1PostExpiredComment() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1PostExpiredComment", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1PostExpiredComment/_expectedQ1.txt", "output.txt");
     }
 
     @Test
     public void Q1PostExpiredComment2() throws IOException, URISyntaxException, InterruptedException {
         Query1 q = new Query1("Q1PostExpiredComment2", "output.txt");
         q.run();
-        Assert.assertTrue(compareFiles("Q1Basic/_expectedQ1.txt", "output.txt"));
+        compareFiles("Q1PostExpiredComment2/_expectedQ1.txt", "output.txt");
     }
 }
